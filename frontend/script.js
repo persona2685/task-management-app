74d393c7318516527fa2b5862ebc267e8fa58699
persona2685 <personasv26@gmail.com>
2026-09-11 23:58:31 +0530

    Connect frontend to deployed backend

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/api/tasks";
+const API_URL = "https://task-management-app-j2ji.onrender.com/api";
 const token = localStorage.getItem("token");
 
 if (!token) {
